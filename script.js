b30248a056b132911ba45eb737cc6bb97e0c2e2c
Raj Sah Rauniyar <113820963+art-rauniyar@users.noreply.github.com>
2023-03-12 01:56:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd2d7d7ce8cmshd034ae77114e3eep1e5c23jsn67930b90b5ba',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getWeather = (city) => {
+    cityName.innerHTML = city
+
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+
+
+            console.log(response)
+
+            // cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            temp2.innerHTML = response.temp
+
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            humidity2.innerHTML = response.humidity
+
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+
+            wind_speed.innerHTML = response.wind_speed
+            wind_speed2.innerHTML = response.wind_speed
+
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+        })
+        .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Kathmandu")
